2bbec6c3853774e2861c91f041616e7ca2587cb3
No-YE <nye7181@gmail.com>
2019-03-19 23:41:14 +0900

    deploy.sh 테스트 위해 수정

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,14 +2,4 @@
 
 cd /home/ubuntu/deploy
 
-if [[ "$(docker ps -a -q)" != "" ]]; then
-  docker rm -f $(docker ps -a -q)
-fi
-if [[ "$(docker images -q)" != "" ]]; then
-  docker rmi -f $(docker images -q)
-fi
-
-docker build -t app .
-docker run -p 3000:3000 -d app
-
-docker rmi $(docker images -f "dangling=true" -q)
+echo helloooooooooooooooooooworld
