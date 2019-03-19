5488ccd8d087835e84d0594135bafe8007126ece
No-YE <nye7181@gmail.com>
2019-03-20 07:37:37 +0900

    deploy.sh if문의 괄호 한 겹 없앰

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,4 +2,14 @@
 
 cd /home/ubuntu/deploy
 
-echo helloooooooooooooooooooworld
+if [ "$(docker ps -a -q)" != "" ]; then
+  docker rm -f $(docker ps -a -q)
+fi
+if [ "$(docker images -q)" != "" ]; then
+  docker rmi -f $(docker images -q)
+fi
+
+docker build -t app .
+docker run -p 3000:3000 -d app
+
+docker rmi $(docker images -f "dangling=true" -q)
