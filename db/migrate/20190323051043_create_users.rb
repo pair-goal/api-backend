34a160942a3d3fcc92998b86918015b8cae677af
No-YE <nye7181@gmail.com>
2019-03-23 15:09:00 +0900

    user 테이블에서 name을 nickname으로 수정

--- db/migrate/20190323051043_create_users.rb
+++ db/migrate/20190323051043_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :nickname
       t.string :password
       t.string :image_path
       t.text :description
