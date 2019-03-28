8f7cfc5693a2fc4966250d5f89c6120bc9332d1a
No-YE <nye7181@gmail.com>
2019-03-28 19:03:02 +0900

    user valid 테스트 추가

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "user save" do
+  test "user valid save" do
     user = User.new({
       nickname: "NoYE",
       password: "abcd1234",
@@ -9,5 +9,29 @@ class UserTest < ActiveSupport::TestCase
     })
 
     assert user.save, "Failed to save"
+  end
+
+  test "user invalid save" do
+    long_nickname_user = User.new({
+      nickname: "abcdefghijklmnopqrstuvwxyz",
+      password: "abcd1234",
+      description: "hello"
+    })
+
+    short_password_user = User.new({
+      nickname: "NoYE",
+      password: "aa",
+      description: "hello"
+    })
+
+    long_password_user = User.new({
+      nickname: "NoYE",
+      password: "abcdefghijklmnopqrstuvwxyz",
+      description: "hello"
+    })
+
+    assert !long_nickname_user.save, "Failed to long nickname validate"
+    assert !short_password_user.save, "Failed to short password validate"
+    assert !long_password_user.save, "Failed to long password validate"
   end
 end
