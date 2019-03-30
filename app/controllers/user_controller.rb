e412039e017baa90dcd6561af27f58e66d26bb71
No-YE <nye7181@gmail.com>
2019-03-30 22:25:30 +0900

    sign up 구현

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,17 @@
 class UserController < ApplicationController
+  def create
+    params.require(:user).permit(:nickname, :password)
+
+    if User.exists?(params[:nickname])
+      render json: {message: 'error'}, status: 405
+    else
+      user = User.new({
+        nickname: params[:nickname],
+        password: params[:password]
+      })
+
+      user.save
+      render json: {message: 'created'}, status: 201      
+    end
+  end
 end
