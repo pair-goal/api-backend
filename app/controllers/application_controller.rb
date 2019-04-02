1d301ba54a6700af1a594857b09842f330420a86
No-YE <nye7181@gmail.com>
2019-04-02 15:40:58 +0900

    토큰 인증 함수 구현

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  def auth_user
+    token = request.headers["Access-Token"]
+    decoded_token = JsonWebToken.decode(token)
+    
+    if decoded_token==false
+      render json: {message: 'error'}, status: 403
+    end
+  end
 end
