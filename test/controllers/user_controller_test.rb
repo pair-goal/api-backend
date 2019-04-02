9a9b8996aa9a0f09704b26de7f6d36f619105b3d
No-YE <nye7181@gmail.com>
2019-04-02 11:20:49 +0900

    signin 테스트 추가

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -18,4 +18,22 @@ class UserControllerTest < ActionDispatch::IntegrationTest
 
     assert_response 405
   end
+
+  test "success signin" do
+    post '/user/signin', params: { nickname: "NoYE", password: "duddms7181" }, as: :json
+
+    assert_response 200
+  end
+
+  test "fail signin - not exist nickname" do
+    post '/user/signin', params: { nickname: "NoYEaaa", password: "duddms7181" }, as: :json
+
+    assert_response 405
+  end
+
+  test "fail signin - wrong password" do
+    post '/user/signin', params: { nickname: "NoYE", password: "abcd234d" }, as: :json
+
+    assert_response 405
+  end
 end
