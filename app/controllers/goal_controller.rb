3a79a2f811bf7510c3a4856d29faf3001d0cb7ce
No-YE <nye7181@gmail.com>
2019-04-04 22:53:32 +0900

    goal 테스트 코드 작성

---
+++ app/controllers/goal_controller.rb
@@ -0,0 +1,18 @@
+class GoalController < ApplicationController
+  before_action :auth_user
+
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
