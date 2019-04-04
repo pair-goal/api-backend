7ca37e311a0283503563000da0983377f84116f7
No-YE <nye7181@gmail.com>
2019-04-04 23:34:55 +0900

    get goals 구현

--- app/controllers/goal_controller.rb
+++ app/controllers/goal_controller.rb
@@ -2,6 +2,10 @@ class GoalController < ApplicationController
   before_action :auth_user
 
   def index
+    doing, offset, limit = params.values_at :doing, :offset, :limit
+    doing = doing == "true" ? true : false
+
+    @goals = Goal.where(is_doing: doing).offset(offset).limit(limit)
   end
 
   def create
