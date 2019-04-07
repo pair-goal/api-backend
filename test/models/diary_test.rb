d9ddea767ff677b3f91562a86d0fa2038a96480f
No-YE <nye7181@gmail.com>
2019-04-07 21:21:06 +0900

    diary model 테스트 작성

--- test/models/diary_test.rb
+++ test/models/diary_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class DiaryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "diary valid save" do
+    diary = Diary.new({
+      goal_id: Goal.where(id: 1).first.id,
+      comment: "success!",
+      score: 5
+    })
+
+    assert diary.save, "Failed to save"
+  end
+
+  test "find diary test" do
+    diary = Diary.where(id: 1).first
+
+    assert_instance_of Diary, diary, "diary is not instance of Diary"
+    assert_equal diaries(:one).comment, diary.comment
+  end
 end
