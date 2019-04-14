90cfbb9db383a7be099b903504abd8500d3aca30
No-YE <nye7181@gmail.com>
2019-04-14 20:40:19 +0900

    redis publish 변경

--- app/workers/matching_worker.rb
+++ app/workers/matching_worker.rb
@@ -2,17 +2,25 @@ class MatchingWorker
   include Sidekiq::Worker
 
   def perform
-    Category.constants(false).each { |category| matching category }
+    Category.constants(false).each { |c| matching Category.const_get c }
   end
 
   def matching(category)
+    $redis.select(3)
     len = $redis.llen(category)/2
-    len.times {
+    len.times do
       first = Goal.where(id: $redis.rpop(category)).first
       second = Goal.where(id: $redis.rpop(category)).first
 
       first.update(partner_id: second.user_id)
       second.update(partner_id: first.user_id)
-    }
+
+      conversation = [
+        {nickname: first.user_id.nickname, title: first.title},
+        {nickname: second.user_id.nickname, title: second.title}
+      ].to_json
+
+      $redis.publish('newConversation', conversation)
+    end
   end
 end
