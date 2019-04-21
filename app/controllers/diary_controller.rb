ec5e03f6e9d8e8e867ba11ec7871d758640cf621
No-YE <nye7181@gmail.com>
2019-04-21 20:07:39 +0900

    diary 평균 점수 계싼

--- app/controllers/diary_controller.rb
+++ app/controllers/diary_controller.rb
@@ -25,8 +25,17 @@ class DiaryController < ApplicationController
 
     if(Diary.exists?(goal_id: goal_id, date: today))
       diary = Diary.where(goal_id: goal_id, date: today).first
+      goal = Goal.where(id: diary.goal_id).first
 
-      if(diary.update({score: score, comment: comment}))
+      pre_score = diary.score
+
+      score_column = int_to_column(score.to_i)
+      pre_score_column = int_to_column(pre_score)
+      
+      score_num = goal[score_column]
+      pre_score_num = goal[pre_score_column]
+
+      if(diary.update({score: score, comment: comment}) && goal.update(score_column => score_num+1, pre_score_column => pre_score_num-1))
         head 201
       else
         head 405
@@ -34,6 +43,11 @@ class DiaryController < ApplicationController
     else
       goal = Goal.where(id: goal_id).first
       last_diary = Diary.where(id: goal.last_diary_id).first
+
+      unless last_diary
+        head 201
+        return
+      end
 
       diary = {
         goal_id: goal_id,
@@ -52,6 +66,17 @@ class DiaryController < ApplicationController
       else
         head 405
       end
+    end
+  end
+
+  def int_to_column(number)
+    case number
+      when 0 then return :zero_score
+      when 1 then return :one_score
+      when 2 then return :two_score
+      when 3 then return :three_score
+      when 4 then return :four_score
+      when 5 then return :five_score
     end
   end
 end
