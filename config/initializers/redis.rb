09354fae50551724d30babd917d724e518539021
No-YE <nye7181@gmail.com>
2019-06-04 10:52:11 +0900

    redis subscribe newConversation 추가

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,3 +2,7 @@ require 'redis'
 
 Redis::Client::DEFAULTS[:host] = ENV['REDIS_HOST']
 $redis = Redis.new(host: ENV['REDIS_HOST'], port: ENV['REDIS_PORT'])
+
+$redis.subscribe "newConversation" do |data|
+  Goal.where(id: data.goal_id).first.update(conversation_id: data.conversation_id)
+end
